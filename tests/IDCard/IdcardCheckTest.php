1e6625ee31924adaf3e5bc79d14965850c550cfe
PeterWan <76828528@qq.com>
2018-03-17 15:36:25 +0800

    创建单元测试，调整开发信息
    创建单元测试，调整开发信息

---
+++ tests/IDCard/IdcardCheckTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 2018/3/17
+ * Time: 上午11:52
+ */
+
+namespace Idcard\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class IdcardCheckTest extends TestCase
+{
+    protected function setUp()
+    {
+        global $idcard;
+    }
+
+    function testCheck()
+    {
+//        $idcard->check
+    }
+
+
+}
